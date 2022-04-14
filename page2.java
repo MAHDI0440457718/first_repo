ad683c2be8f88c54b68441fcb0e37105e23e44aa
hach mamad <gagalsag@gmail.com>
2022-04-14 16:05:24 +0430

    taghirato too repo ok kardim

--- page2.java
+++ page2.java
@@ -3,6 +3,6 @@ public class Main {
 
     public static void main(String[] args) {
         //in commende branche
-	System.out.println("nahar kabab");
+	System.out.println("nahar kabab /n in matn ra mn az repository gereftam/n alan daram taghir midam");
     }
 }
