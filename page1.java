4b21be2cd327cf15431d4ea46b13da18603214f8
MAHDI0440457718 <103611136+MAHDI0440457718@users.noreply.github.com>
2022-04-15 16:33:28 +0430

    dakhele git taghir ddam

--- page1.java
+++ page1.java
@@ -1,6 +1,6 @@
 public class Main {
 
-
+ //dakhele github in khato neveshtam
     public static void main(String[] args) {
 	//in commende branche mn ino too beranche fix neveshtam
         System.out.println("soboone kalapch");
