ca2d5a0a1148e69402481826f4ce8e5839baba9f
MAHDI0440457718 <103611136+MAHDI0440457718@users.noreply.github.com>
2022-04-16 12:39:24 +0430

    test baraye inke yek faile avaz she

--- page1.java
+++ page1.java
@@ -1,5 +1,5 @@
 public class Main {
-
+	//test baraye inke yek file avz she
  //dakhele github in khato neveshtam
     public static void main(String[] args) {
 	//in commende branche mn ino too beranche fix neveshtam
